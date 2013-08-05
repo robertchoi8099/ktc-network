c5895aa5b34804d676b7eded1274537f366206c3
Wil Reichert <wil.reichert@kt.com>
2013-08-05 22:48:30 +0900

    configure rabbit server and service endpoint

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,13 @@
 ## Recipe:: default
 ##
 
+class Chef::Recipe
+  include KTCUtils
+end
+
+set_rabbit_servers "network"
+set_service_endpoint_ip "network-api"
+
 include_recipe "openstack-common"
 include_recipe "openstack-common::logging"
 include_recipe "openstack-network::server"
