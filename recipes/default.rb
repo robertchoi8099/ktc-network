f06703c7e2d74b0615f7f1fa0a881b987671f6df
Wil Reichert <wil.reichert@kt.com>
2013-08-19 23:50:14 +0900

    add etcd service discovery

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,9 @@ class Chef::Recipe
   include KTCUtils
 end
 
+d = get_openstack_service_template(get_interface_address("management"), "9696")
+register_service("network-api", d)
+
 set_rabbit_servers "network"
 set_service_endpoint_ip "network-api"
 
