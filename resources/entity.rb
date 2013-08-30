647b935a8e1bae2aa1b7aab7674fd53a08456017
Chaman <chaman.kang@kt.com>
2013-08-30 19:19:14 +0900

    integrate router creation to ktc_network_entity resource

--- resources/entity.rb
+++ resources/entity.rb
@@ -1,8 +1,10 @@
-actions :create
+actions :create_router, :create_network
 
 attribute :auth_uri, :kind_of => String, :required => true
 attribute :tenant_name, :kind_of => String, :required => true
 attribute :user_name, :kind_of => String, :required => true
 attribute :user_pass, :kind_of => String, :required => true
+attribute :router_name, :kind_of => String, :default => nil
+attribute :options,   :kind_of => Hash, :default => {}
 
-attr_accessor :router
+attr_accessor :entity
