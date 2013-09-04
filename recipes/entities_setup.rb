063db629b0191ad0bfac4e2b3be4e91d08a4a872
Chaman <chaman.kang@kt.com>
2013-09-04 18:55:25 +0900

    add entities_setup recipe

--- recipes/entities_setup.rb
+++ recipes/entities_setup.rb
@@ -7,28 +7,44 @@ auth_uri = ::URI.decode identity_endpoint.to_s
 service_pass = service_password "openstack-network"
 
 physical_net = node["openstack"]["network"]["linuxbridge"]["physical_network"] 
-ktc_network_entity physical_net do
+network_id = node["openstack"]["network"]["l3"]["network_id"]
+ktc_network_network physical_net do
   auth_uri    auth_uri
   user_pass   service_pass
   tenant_name node["openstack"]["network"]["service_tenant_name"]
   user_name   node["openstack"]["network"]["service_user"]
   options(
-    :name => physical_net,
-    :multihost_multi_host => true,
-    :provider_network_type => "flat",
-    :provider_physical_network => physical_net,
-    :shared => true
+    "name" => physical_net,
+    "multihost:multi_host" => true,
+    "provider:network_type" => "flat",
+    "provider:physical_network" => physical_net,
+    "shared" => true
   )
-  action :create_network
+  action :create
 end
 
-ktc_network_entity node["openstack"]["network"]["l3"]["router_name"] do
+ktc_network_router node["openstack"]["network"]["l3"]["router_name"] do
   auth_uri    auth_uri
   user_pass   service_pass
   tenant_name node["openstack"]["network"]["service_tenant_name"]
   user_name   node["openstack"]["network"]["service_user"]
   options(
-    :multihost_network_id => node["openstack"]["network"]["l3"]["network_id"]
+    "name" => node["openstack"]["network"]["l3"]["router_name"],
+    "multihost:network_id" => network_id
   )
-  action :create_router
+  action :create
+end
+
+ktc_network_subnet node["openstack"]["network"]["linuxbridge"]["physical_subnet"] do
+  auth_uri    auth_uri
+  user_pass   service_pass
+  tenant_name node["openstack"]["network"]["service_tenant_name"]
+  user_name   node["openstack"]["network"]["service_user"]
+  options(
+    "network_id" => network_id,
+    "cidr" => node["openstack"]["network"]["linuxbridge"]["physical_cidr"],
+    "dns_nameservers" => node["openstack"]["network"]["linuxbridge"]["dns_nameservers"],
+    "name" => node["openstack"]["network"]["linuxbridge"]["physical_subnet"]
+  )
+  action :create
 end
