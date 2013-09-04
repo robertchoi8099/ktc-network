6f64bd56664e16211009512b3f5db250d49613de
Chaman <chaman.kang@kt.com>
2013-09-04 16:19:11 +0900

    add ktc_network_subnet resource

---
+++ resources/subnet.rb
@@ -0,0 +1,9 @@
+actions :create
+
+attribute :auth_uri, :kind_of => String, :required => true
+attribute :tenant_name, :kind_of => String, :required => true
+attribute :user_name, :kind_of => String, :required => true
+attribute :user_pass, :kind_of => String, :required => true
+attribute :options,   :kind_of => Hash, :default => {}
+
+attr_accessor :entity
