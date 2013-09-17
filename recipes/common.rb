39aa2995c4e6d9a13510e2efbd14197c7e27e631
chamankang <chaman.kang@kt.com>
2013-09-17 15:10:28 +0900

    use the upstream linuxbridge_conf template
    IptableFirewallDriver is now configured in the upstream template

--- recipes/common.rb
+++ recipes/common.rb
@@ -12,18 +12,3 @@ require 'chef/rewind'
 rewind :template => "/etc/quantum/quantum.conf" do
   cookbook_name "ktc-network"
 end
-
-platform_options = node["openstack"]["network"]["platform"]
-
-driver_name = node["openstack"]["network"]["interface_driver"]
-driver_name = driver_name.split('.').last.downcase
-main_plugin = node["openstack"]["network"]["interface_driver_map"][driver_name]
-
-case main_plugin
-when "linuxbridge"
-
-  rewind :template => "/etc/quantum/plugins/linuxbridge/linuxbridge_conf.ini" do
-    cookbook_name "ktc-network"
-  end
-
-end
