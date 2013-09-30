af14b517bf53dd86cef639649c4a23e6baf940cc
chamankang <chaman.kang@kt.com>
2013-09-30 16:15:35 +0900

    set quantum-server bind address to the address of bind_interface

--- recipes/common.rb
+++ recipes/common.rb
@@ -3,6 +3,11 @@
 # Recipe:: common
 #
 
+include_recipe "ktc-utils"
+
+iface = KTC::Network.if_lookup "management"
+node.set["openstack"]["network"]["api"]["bind_interface"] = iface
+
 include_recipe "ktc-network::source_install"
 include_recipe "openstack-network::common"
 
