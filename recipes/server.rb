df0d0256b3743a54a38ab6cc66bbaa62e1a21510
Robert <taeil.choi@kt.com>
2013-10-28 17:12:29 +0900

    add process monitoring

--- recipes/server.rb
+++ recipes/server.rb
@@ -39,3 +39,20 @@ rewind :service => "quantum-server" do
   provider Chef::Provider::Service::Upstart
   action [:enable, :start]
 end
+
+# process monitoring and sensu-check config
+processes = node['openstack']['network']['server_processes']
+
+processes.each do |process|
+  sensu_check "check_process_#{process['name']}" do
+    command "check-procs.rb -c 10 -w 10 -C 1 -W 1 -p #{process['name']}"
+    handlers ["default"]
+    standalone true
+    interval 20
+  end
+end
+
+collectd_processes "quantum-server-processes" do
+  input processes
+  key "shortname"
+end
