ddfda2a815fb3e134463a4d84291418c3ecc2647
chamankang <chaman.kang@kt.com>
2013-10-31 19:09:50 +0900

    add attrs for ipc-stage and configs for heartbeat and single zone network

--- recipes/setup_entities.rb
+++ recipes/setup_entities.rb
@@ -8,8 +8,17 @@ service_pass = service_password "openstack-network"
 tenant_name = node["openstack"]["network"]["service_tenant_name"]
 user_name = node["openstack"]["network"]["service_user"]
 
-private_net = node["openstack"]["network"]["ng_l3"]["private_network"] ||
-  node["openstack"]["network"]["linuxbridge"]["physical_network"]
+heartbeat_net = node["openstack"]["network"]["ng_l3"]["heartbeat_network"]
+heartbeat_subnet = node["openstack"]["network"]["ng_l3"]["heartbeat_subnet"]
+heartbeat_cidr = node["openstack"]["network"]["ng_l3"]["heartbeat_cidr"]
+heartbeat_nameservers = node["openstack"]["network"]["ng_l3"]["heartbeat_nameservers"]
+heartbeat_gateway_ip = node["openstack"]["network"]["ng_l3"]["heartbeat_gateway_ip"]
+heartbeat_net_options = {
+  "name" => heartbeat_net,
+  "multihost:multi_host" => true,
+  "shared" => true
+}
+private_net = node["openstack"]["network"]["ng_l3"]["private_network"]
 private_subnet = node["openstack"]["network"]["ng_l3"]["private_subnet"]
 private_cidr = node["openstack"]["network"]["ng_l3"]["private_cidr"]
 private_nameservers = node["openstack"]["network"]["ng_l3"]["private_nameservers"]
@@ -20,6 +29,33 @@ private_net_options = {
   "multihost:multi_host" => true,
   "shared" => true
 }
+
+ktc_network_network "Create Heartbeat Network" do
+  auth_uri    auth_uri
+  user_pass   service_pass
+  tenant_name tenant_name
+  user_name   user_name
+  options     heartbeat_net_options
+  action :create
+end
+
+ktc_network_subnet "Create Heartbeat Subnet" do
+  auth_uri    auth_uri
+  user_pass   service_pass
+  tenant_name tenant_name
+  user_name   user_name
+  search_id(
+    :network => heartbeat_net_options
+  )
+  options(
+    "network_id" => :network,
+    "cidr" => heartbeat_cidr,
+    "dns_nameservers" => heartbeat_nameservers,
+    "name" => heartbeat_subnet,
+    "gateway_ip" => heartbeat_gateway_ip
+  )
+  action :create
+end
 
 ktc_network_network "Create Private Network" do
   auth_uri    auth_uri
