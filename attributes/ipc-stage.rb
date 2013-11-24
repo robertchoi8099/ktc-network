45442c2f3d76edc4137899b326c7c6253f39643a
chamankang <chaman.kang@kt.com>
2013-11-24 17:36:10 +0900

    fix ipc-stage ng_l3 attrs to new style

--- attributes/ipc-stage.rb
+++ attributes/ipc-stage.rb
@@ -3,16 +3,43 @@ return  unless  chef_environment == "ipc-stage"
 include_attribute "ktc-network::ng_l3"
 
 # override this like the example below
-default["openstack"]["network"]["ng_l3"] = {
-  "setup_entities" => true,
-  "heartbeat_network" => "heartbeat",
-  "heartbeat_subnet" => "heartbeat",
-  "heartbeat_cidr" => "10.210.11.0/24",
-  "heartbeat_nameservers" => ["8.8.8.8"],
-  "heartbeat_gateway_ip" => :null,
-  "private_network" => "ipc-stage",
-  "private_subnet" => "ipc-stage",
-  "private_cidr" => "10.210.10.0/24",
-  "private_nameservers" => ["8.8.8.8"],
-  "private_gateway_ip" => :null
-}
+default["openstack"]["network"]["ng_l3"]["setup_entities"] = true
+default["openstack"]["network"]["ng_l3"]["private_network"] = "ipc-stage"
+default["openstack"]["network"]["ng_l3"]["networks"] = [
+  {
+    "options" => {
+      "name" => "heartbeat",
+      "multihost:multi_host" => true,
+      "shared" => true
+    }
+  },
+  {
+    "options" => {
+      "name" => "ipc-stage",
+      "multihost:multi_host" => true,
+      "shared" => true
+    }
+  }
+]
+default["openstack"]["network"]["ng_l3"]["subnets"] = [
+  {
+    "search_id" => { :network => { "name" => "heartbeat" } },
+    "options" => {
+      "network_id" => :network,
+      "name" => "heartbeat",
+      "cidr" => "10.210.11.0/24",
+      "dns_nameservers" => ["8.8.8.8"],
+      "gateway_ip" => :null
+    }
+  },
+  {
+    "search_id" => { :network => { "name" => "ipc-stage" } },
+    "options" => {
+      "network_id" => :network,
+      "name" => "ipc-stage",
+      "cidr" => "10.210.10.0/24",
+      "dns_nameservers" => ["8.8.8.8"],
+      "gateway_ip" => :null
+    }
+  }
+]
